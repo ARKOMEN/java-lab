0db92dacf399a6c5f17604fcc75a52f4a43d4b84
artemiy <Koshevoyak@yandex.ru>
2024-05-14 21:21:35 +0700

    multithreading on server

---
+++ lab5/src/main/java/ru/nsu/koshevoi/lab5/server/ClientHandler.java
@@ -0,0 +1,37 @@
+package ru.nsu.koshevoi.lab5.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler extends Thread{
+    private Socket socket;
+
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)){
+
+            String clientMessage;
+            while((clientMessage = reader.readLine()) != null){
+                System.out.println("Сообщение от клиента " + clientMessage);
+                writer.println("Эхо: " + clientMessage);
+            }
+        } catch (IOException e){
+            System.out.println("ошибка в работе с клиентом " + e.getMessage());
+        } finally {
+            try{
+                socket.close();
+            }catch (IOException e){
+                System.out.println("Ошибка при закрытии сокета: " + e.getMessage());
+            }
+        }
+
+    }
+}
--- lab5/src/main/java/ru/nsu/koshevoi/lab5/server/Server.java
+++ lab5/src/main/java/ru/nsu/koshevoi/lab5/server/Server.java
@@ -1,4 +1,34 @@
 package ru.nsu.koshevoi.lab5.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private int port;
+
+    public Server(int port){
+        this.port = port;
+    }
+
+    public void start() {
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Сервер запущен на порту: " + port);
+
+            while(true){
+                Socket socket = serverSocket.accept();
+                System.out.println("Новое подключение: " + socket.getInetAddress());
+                ClientHandler clientHandler = new ClientHandler(socket);
+                clientHandler.start();
+            }
+        }catch (IOException e){
+            System.out.println("Ошибка при работе сервера: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args){
+        int port = 1234;
+        Server server = new Server(port);
+        server.start();
+    }
 }
